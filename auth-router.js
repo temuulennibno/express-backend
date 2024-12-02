e3adac1404445243df5f1593e4bdb5635bb5040c
Temuulen Narmandakh <temuulen.n@pinecone.mn>
2024-12-02 18:32:03 +0800

    signup & signin final

--- auth-router.js
+++ auth-router.js
@@ -5,14 +5,10 @@ import UserModel from "./models/user-model.js";
 const router = express.Router();
 
 const checkIsPhoneNumber = (credential) => {
-  console.log("1");
   if (credential.length !== 8) return false;
-  console.log("2");
   if (isNaN(Number(credential))) return false;
-  console.log("3");
   const firstCharacter = credential[0];
   if (!["9", "8", "7", "6"].includes(firstCharacter)) return false;
-  console.log("4");
   return true;
 };
 
@@ -37,23 +33,65 @@ router.post("/signup", async (req, res) => {
   }
   // BUH TALBAR UTGATAI BAIGAA
 
+  if (password.length < 7) {
+    return res.status(400).send({ message: "Ta 8 buyu tuunees deesh temdegttei password hiine uu!" });
+  }
+
+  const existingUser = await UserModel.findOne({ username: username });
+  if (existingUser) {
+    return res.status(400).send({ message: "Username burtgeltei baina!" });
+  }
+
   const isPhoneNumber = checkIsPhoneNumber(credential);
   const isEmail = checkIsEmail(credential);
 
-  const existingUser = await UserModel.findOne({ email });
-  if (existingUser) return res.status(400).send({ message: "Email already registered!" });
+  if (!isPhoneNumber && !isEmail) {
+    return res.status(400).send({ message: "Ta zovhon utasnii dugaar esvel email oruulna uu!" });
+  }
 
-  bcrypt.hash(password, 10, async function (err, hash) {
-    const newUser = { email, password: hash };
-    await UserModel.create(newUser);
-    return res.status(201).send(newUser);
-  });
+  if (isPhoneNumber) {
+    const existingUser = await UserModel.findOne({ phone: credential });
+    if (existingUser) {
+      return res.status(400).send({ message: "Utasnii dugaar burtgeltei baina!" });
+    } else {
+      bcrypt.hash(password, 10, async function (err, hash) {
+        const newUser = { phone: credential, fullname: fullname, password: hash, username: username };
+        await UserModel.create(newUser);
+        return res.status(201).send(newUser);
+      });
+    }
+  }
+
+  if (isEmail) {
+    const existingUser = await UserModel.findOne({ email: credential });
+    if (existingUser) {
+      return res.status(400).send({ message: "Email burtgeltei baina!" });
+    } else {
+      bcrypt.hash(password, 10, async function (err, hash) {
+        const newUser = { email: credential, fullname: fullname, password: hash, username: username };
+        await UserModel.create(newUser);
+        return res.status(201).send(newUser);
+      });
+    }
+  }
 });
 
 router.post("/signin", async (req, res) => {
-  const { email, password } = req.body;
-  const existingUser = await UserModel.findOne({ email });
-  if (!existingUser) return res.status(400).send({ message: "Email or password not correct!" });
+  const { credential, password } = req.body;
+
+  let existingUser = null;
+  if (checkIsPhoneNumber(credential)) {
+    existingUser = await UserModel.findOne({ phone: credential });
+  } else if (checkIsEmail(credential)) {
+    existingUser = await UserModel.findOne({ email: credential });
+  } else {
+    existingUser = await UserModel.findOne({ username: credential });
+  }
+
+  if (!existingUser) {
+    return res.status(400).send({ message: "Credential or password not correct!" });
+  }
+
   bcrypt.compare(password, existingUser.password, function (err, result) {
     if (!result) {
       return res.status(400).send({ message: "Email or password not correct!" });
--- models/user-model.js
+++ models/user-model.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, unique: true },
-  phone: { type: String, unique: true },
+  email: { type: String },
+  phone: { type: String },
   fullname: { type: String, required: true },
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
