7f313c5b9fb7c23439eabd0ff6866f22f736e473
Temuulen Narmandakh <temuulen.n@pinecone.mn>
2024-12-16 17:01:17 +0800

    add posts get request

--- routers/post-router.js
+++ routers/post-router.js
@@ -3,6 +3,11 @@ import PostModel from "../models/post-model.js";
 
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+  const posts = await PostModel.find({}).sort({ createdAt: -1 });
+  return res.send(posts);
+});
+
 router.post("/", async (req, res) => {
   const { description } = req.body;
   const newPost = await PostModel.create({ description });
