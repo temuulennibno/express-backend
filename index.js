17166bb61e7d0f115afd1a7603269378376fdd7d
Temuulen Narmandakh <temuulen.n@pinecone.mn>
2024-12-18 18:32:11 +0800

    add multer & cloudinary

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import mongoose from "mongoose";
 import authRouter from "./routers/auth-router.js";
 import postRouter from "./routers/post-router.js";
+import fileRouter from "./routers/file-router.js";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -15,6 +16,7 @@ app.use(cors());
 
 app.use(authRouter);
 app.use("/api/posts", postRouter);
+app.use("/api/files", fileRouter);
 
 app.listen(PORT, async () => {
   await mongoose.connect(process.env.DATABASE_URL);
---
+++ routers/file-router.js
@@ -0,0 +1,39 @@
+import express from "express";
+import multer from "multer";
+import { nanoid } from "nanoid";
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: "dqujkx5of",
+  api_key: "697265795575757",
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./images/");
+  },
+  filename: (req, file, cb) => {
+    const uniqueName = `${nanoid()}-${file.originalname}`;
+    cb(null, uniqueName);
+  },
+});
+
+const router = express.Router();
+
+const upload = multer({ storage });
+
+router.use(upload.single("file"));
+
+router.post("/", async (req, res) => {
+  const response = await cloudinary.uploader.upload(req.file.path);
+  return res.send({
+    message: "Uploaded!",
+    fileUrl: response.secure_url,
+  });
+});
+
+export default router;
